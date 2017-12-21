6e003d2be5c24e518ff744b01bcbcc7423ebd453
calvin <calvin.ye@capgemini.com>
2017-12-21 18:16:38 +0800

    commit

---
+++ userms/src/main/java/com/yhever/userms/UserServiceApplication.java
@@ -0,0 +1,34 @@
+package com.yhever.userms;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableEurekaClient
+@EnableAutoConfiguration
+public class UserServiceApplication {
+
+    @RequestMapping("/hi")
+    @ResponseBody
+    String home() {
+        return "Hello World this is user micro service!";
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/welcome")
+    public String welcome(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
